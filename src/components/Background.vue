1a93f49a4fdd4962667d9a5baa4d983916505ad4
lzb446703678 <140237586+lzb446703678@users.noreply.github.com>
2024-03-07 23:00:47 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,7 +39,7 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = `/images/background1.jpg`;
+    bgUrl.value = `https://img.101jc.com/img/background1.jpg!yuantu`;
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
