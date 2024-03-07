3461187b5934e3ee30ddec10dc64ebbe20824371
lzb446703678 <140237586+lzb446703678@users.noreply.github.com>
2024-03-07 23:00:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -52,37 +52,37 @@ export default ({ mode }) =>
           background_color: "#424242",
           icons: [
             {
-              src: "/images/icon/48.png",
+              src: "https://img.101jc.com/img/48.png!yuantu",
               sizes: "48x48",
               type: "image/png",
             },
             {
-              src: "/images/icon/72.png",
+              src: "https://img.101jc.com/img/72.png!yuantu",
               sizes: "72x72",
               type: "image/png",
             },
             {
-              src: "/images/icon/96.png",
+              src: "https://img.101jc.com/img/96.png!yuantu",
               sizes: "96x96",
               type: "image/png",
             },
             {
-              src: "/images/icon/128.png",
+              src: "https://img.101jc.com/img/128.png!yuantu",
               sizes: "128x128",
               type: "image/png",
             },
             {
-              src: "/images/icon/144.png",
+              src: "https://img.101jc.com/img/144.png!yuantu",
               sizes: "144x144",
               type: "image/png",
             },
             {
-              src: "/images/icon/192.png",
+              src: "https://img.101jc.com/img/192.png!yuantu",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/images/icon/512.png",
+              src: "https://img.101jc.com/img/512.png!yuantu",
               sizes: "512x512",
               type: "image/png",
             },
